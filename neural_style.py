22841728d366bc2d3f029627e51869a5572911aa
AlbinJagesten <Albin.Jagesten@gmail.com>
2018-11-09 13:20:01 -0800

    fixing with loss and gram matrix

--- neural_style.py
+++ neural_style.py
@@ -18,7 +18,7 @@ CONTENT_WEIGHT_BLEND = 1
 STYLE_WEIGHT = 5e2
 TV_WEIGHT = 1e2
 STYLE_LAYER_WEIGHT_EXP = 1
-LEARNING_RATE = 1e-5
+LEARNING_RATE = 1e-8
 BETA1 = 0.9
 BETA2 = 0.999
 EPSILON = 1e-08
@@ -29,13 +29,6 @@ POOLING = 'max'
 
 def build_parser():
     parser = ArgumentParser()
-    parser.add_argument('--content',
-            dest='content', help='content image',
-            metavar='CONTENT', required=True)
-    parser.add_argument('--styles',
-            dest='styles',
-            nargs='+', help='one or more style images',
-            metavar='STYLE', required=True)
     parser.add_argument('--output',
             dest='output', help='output path',
             metavar='OUTPUT', required=True)
@@ -114,77 +107,15 @@ def main():
     if not os.path.isfile(options.network):
         parser.error("Network %s does not exist. (Did you forget to download it?)" % options.network)
 
-    content_image = imread(options.content)
-    style_images = [imread(style) for style in options.styles]
-
-    width = options.width
-    if width is not None:
-        new_shape = (int(math.floor(float(content_image.shape[0]) /
-                content_image.shape[1] * width)), width)
-        content_image = scipy.misc.imresize(content_image, new_shape)
-    target_shape = content_image.shape
-    for i in range(len(style_images)):
-        style_scale = STYLE_SCALE
-        if options.style_scales is not None:
-            style_scale = options.style_scales[i]
-        style_images[i] = scipy.misc.imresize(style_images[i], style_scale *
-                target_shape[1] / style_images[i].shape[1])
-
-    style_blend_weights = options.style_blend_weights
-    if style_blend_weights is None:
-        # default is equal weights
-        style_blend_weights = [1.0/len(style_images) for _ in style_images]
-    else:
-        total_blend_weight = sum(style_blend_weights)
-        style_blend_weights = [weight/total_blend_weight
-                               for weight in style_blend_weights]
-
-    initial = options.initial
-    if initial is not None:
-        initial = scipy.misc.imresize(imread(initial), content_image.shape[:2])
-        # Initial guess is specified, but not noiseblend - no noise should be blended
-        if options.initial_noiseblend is None:
-            options.initial_noiseblend = 0.0
-    else:
-        # Neither inital, nor noiseblend is provided, falling back to random generated initial guess
-        if options.initial_noiseblend is None:
-            options.initial_noiseblend = 1.0
-        if options.initial_noiseblend < 1.0:
-            initial = content_image
-
-    if options.checkpoint_output and "%s" not in options.checkpoint_output:
-        parser.error("To save intermediate images, the checkpoint output "
-                     "parameter must contain `%s` (e.g. `foo%s.jpg`)")
-
-    # try saving a dummy image to the output path to make sure that it's writable
-    if os.path.isfile(options.output) and not options.overwrite:
-        raise IOError("%s already exists, will not replace it without the '--overwrite' flag" % options.output)
-    try:
-        imsave(options.output, np.zeros((500, 500, 3)))
-    except:
-        raise IOError('%s is not writable or does not have a valid file extension for an image file' % options.output)
-
     for iteration, image in stylize(
         network=options.network,
-        initial=initial,
-        initial_noiseblend=options.initial_noiseblend,
-        content=content_image,
-        styles=style_images,
-        preserve_colors=options.preserve_colors,
         iterations=options.iterations,
-        content_weight=options.content_weight,
-        content_weight_blend=options.content_weight_blend,
-        style_weight=options.style_weight,
-        style_layer_weight_exp=options.style_layer_weight_exp,
-        style_blend_weights=style_blend_weights,
-        tv_weight=options.tv_weight,
         learning_rate=options.learning_rate,
         beta1=options.beta1,
         beta2=options.beta2,
         epsilon=options.epsilon,
         pooling=options.pooling,
-        print_iterations=options.print_iterations,
-        checkpoint_iterations=options.checkpoint_iterations
+        print_iterations=options.print_iterations
     ):
         output_file = None
         combined_rgb = image
--- stylize.py
+++ stylize.py
@@ -13,7 +13,8 @@ import vgg
 from load_images import *
 
 
-STYLE_LAYERS = ('relu1_1', 'relu2_1', 'relu3_1', 'relu4_1', 'relu5_1')
+#STYLE_LAYERS = ('relu1_1', 'relu2_1', 'relu3_1', 'relu4_1', 'relu5_1')
+STYLE_LAYERS = ('relu1_1', 'relu2_1')
 
 
 try:
@@ -31,41 +32,17 @@ def print_progress(loss_vals):
         print('{:>13s} {:g}'.format(key + ' loss:', val))
 
 
-def stylize(network, initial, initial_noiseblend, content, styles, preserve_colors, iterations,
-        content_weight, content_weight_blend, style_weight, style_layer_weight_exp, style_blend_weights, tv_weight,
-        learning_rate, beta1, beta2, epsilon, pooling,
-        print_iterations=None, checkpoint_iterations=None):
-    """
-    Stylize images.
-
-    This function yields tuples (iteration, image, loss_vals) at every
-    iteration. However `image` and `loss_vals` are None by default. Each
-    `checkpoint_iterations`, `image` is not None. Each `print_iterations`,
-    `loss_vals` is not None.
-
-    `loss_vals` is a dict with loss values for the current iteration, e.g.
-    ``{'content': 1.23, 'style': 4.56, 'tv': 7.89, 'total': 13.68}``.
-
-    :rtype: iterator[tuple[int,image]]
-    """
-    
-    minibatch_size = 16
-    
-    shape = (1,) + content.shape
-    style_shapes = [(1,) + style.shape for style in styles]
-    style_features = [{} for _ in styles]
+def stylize(network, iterations, learning_rate, beta1, beta2, epsilon, pooling, print_iterations=None, batch_size=8):
 
     vgg_weights, vgg_mean_pixel = vgg.load_net(network)
 
     # Builds the anchor graph
     # Anchor should be the first image in "styles"
     # TODO: test
-    #anchor_graph = tf.Graph()
-    #with anchor_graph.as_default(), anchor_graph.device('/cpu:0'):
 
-    anchor_image = tf.placeholder('float', shape=(minibatch_size, 224,224,3))
-    positive_image = tf.placeholder('float', shape=(minibatch_size, 224,224,3))
-    negative_image = tf.placeholder('float', shape=(minibatch_size, 224,224,3))
+    anchor_image = tf.placeholder('float', shape=(None, 224,224,3))
+    positive_image = tf.placeholder('float', shape=(None, 224,224,3))
+    negative_image = tf.placeholder('float', shape=(None, 224,224,3))
     
     with tf.variable_scope("net", reuse=tf.AUTO_REUSE):
         anchor_net = vgg.net_preloaded(vgg_weights, anchor_image, pooling)
@@ -76,18 +53,17 @@ def stylize(network, initial, initial_noiseblend, content, styles, preserve_colo
     positive_styles = _generate_style(positive_net, STYLE_LAYERS)
     negative_styles = _generate_style(negative_net, STYLE_LAYERS)
 
-    loss_threshold = 100
+    loss_threshold = 1000000000
 
-    # Loss = max(norm(A - P) - norm(A - N) + loss_threshold, 0)
-    loss = tf.maximum(tf.add_n([tf.nn.l2_loss(anchor_styles[layer] - positive_styles[layer]) for layer in STYLE_LAYERS]) \
-                      - tf.add_n([tf.nn.l2_loss(anchor_styles[layer] - negative_styles[layer]) for layer in STYLE_LAYERS]) + loss_threshold, 0)
+    dist_p = tf.add_n([tf.nn.l2_loss(anchor_styles[layer] - positive_styles[layer]) for layer in STYLE_LAYERS])
+    dist_n = tf.add_n([tf.nn.l2_loss(anchor_styles[layer] - negative_styles[layer]) for layer in STYLE_LAYERS])
+    loss = dist_p - dist_n
 
-    
     # optimizer setup
     train_step = tf.train.AdamOptimizer(learning_rate, beta1, beta2, epsilon).minimize(loss)
 
     # Initialize image loader
-    image_loader = Image_Loader('preprocessed_images/', minibatch_size)
+    image_loader = Image_Loader('preprocessed_images/', batch_size)
     
     # optimization
     best_loss = float('inf')
@@ -114,19 +90,16 @@ def stylize(network, initial, initial_noiseblend, content, styles, preserve_colo
             else:
                 print('Iteration %4d/%4d' % (i + 1, iterations))
 
-            style_pre0 = np.array([vgg.preprocess(styles[0], vgg_mean_pixel)])
-            style_pre1 = np.array([vgg.preprocess(styles[1], vgg_mean_pixel)])
-            style_pre2 = np.array([vgg.preprocess(styles[2], vgg_mean_pixel)])
-
             anchor, positive, negative = np.array(image_loader.load_next_batch())
 
-            anchor = np.array([vgg.preprocess(anchor, vgg_mean_pixel)])
-            positive = np.array([vgg.preprocess(positive, vgg_mean_pixel)])
-            negative = np.array([vgg.preprocess(negative, vgg_mean_pixel)])
-            
-            train_step.run(feed_dict={anchor_image : anchor, positive_image: positive, negative_image: negative})
-            print(loss.eval(feed_dict={anchor_image : anchor, positive_image: positive, negative_image: negative}))
+            anchor = vgg.preprocess(anchor, vgg_mean_pixel)
+            negative = vgg.preprocess(positive, vgg_mean_pixel)
+            positive = vgg.preprocess(negative, vgg_mean_pixel)
 
+            _, cost, cost2 = sess.run([train_step, dist_p, dist_n], feed_dict={anchor_image : anchor, positive_image: positive, negative_image: negative})
+            print(cost, cost2)
+            #train_step.run(feed_dict={anchor_image : anchor, positive_image: positive, negative_image: negative})
+            #cost = sess.run([dist], feed_dict={anchor_image : anchor, positive_image: negative, negative_image: positive})
 
             """
             last_step = (i == iterations - 1)
@@ -148,24 +121,11 @@ def _generate_style(net, style_layers):
 
     for layer in style_layers:
         features = net[layer]
-        features = tf.reshape(features, [-1, features.shape[3]])
-        gram = tf.divide(tf.matmul(tf.transpose(features), features), tf.size(features, out_type=tf.float32))
+        features = tf.reshape(features, [-1, features.shape[1] * features.shape[2], features.shape[3]])
+        gram = tf.divide(tf.matmul(tf.transpose(features, perm=[0,2,1]), features), tf.size(features[0], out_type=tf.float32))
         styles[layer] = gram
 
     return styles
-
-def _tensor_size(tensor):
-    from operator import mul
-    return reduce(mul, (d.value for d in tensor.get_shape()), 1)
-
-def rgb2gray(rgb):
-    return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
-
-def gray2rgb(gray):
-    w, h = gray.shape
-    rgb = np.empty((w, h, 3), dtype=np.float32)
-    rgb[:, :, 2] = rgb[:, :, 1] = rgb[:, :, 0] = gray
-    return rgb
 
 def hms(seconds):
     seconds = int(seconds)
